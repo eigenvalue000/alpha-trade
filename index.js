b5fc5339859d3d1e05a751ec593e61f28f0a0c88
BuildTools <unconfigured@null.spigotmc.org>
2024-12-01 18:01:42 -0800

    Added FetchEtfTickers.

--- index.js
+++ index.js
@@ -10,9 +10,12 @@ const OptionContractSnapshot = require("./lib/Polygon/OptionContractSnapshot");
 const FetchOptionTickers = require("./lib/Polygon/FetchOptionTickers");
 const FetchStockTickers = require("./lib/Polygon/FetchStockTickers");
 const FetchCompanyInfo = require("./lib/Polygon/FetchCompanyInfo");
+const FetchEtfTickers = require("./lib/Polygon/FetchEtfTickers");
 
 module.exports = { FetchAggregates, 
     OptionContractSnapshot, 
     FetchOptionTickers, 
     FetchStockTickers,
-    FetchCompanyInfo };
+    FetchCompanyInfo,
+    FetchEtfTickers
+};
